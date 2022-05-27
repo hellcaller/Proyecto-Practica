efc07713fc9ca176394a243b35947dc8f2cc2387
Gabriel Felipe Aguilar <gabrielfe4@gmail.com>
2022-05-27 15:24:48 -0500

    cambio docker compose

--- docker.ps1
+++ docker.ps1
@@ -1,7 +1,7 @@
-docker build  --no-cache -t microservicios-eureka:1 ./microservicios-eureka
-docker build  --no-cache -t microservicios-examenes:1 ./microservicios-examenes
-docker build  --no-cache -t microservicios-respuestas:1 ./microservicios-respuestas
-docker build  --no-cache -t microservicios-cursos:1 ./microservicios-cursos
-docker build  --no-cache -t microservicios-usuarios:1 ./microservicios-usuarios
-docker build  --no-cache -t microservicios-gateway:1 ./microservicios-gateway
-docker build  --no-cache -t frontend:1 ./frontEnd
+docker build  --no-cache -t hellcaller/practica:microservicios-eureka  ./microservicios-eureka
+docker build  --no-cache -t  hellcaller/practica:microservicios-examenes ./microservicios-examenes
+docker build  --no-cache -t  hellcaller/practica:microservicios-respuestas ./microservicios-respuestas
+docker build  --no-cache -t  hellcaller/practica:microservicios-cursos ./microservicios-cursos
+docker build  --no-cache -t  hellcaller/practica:microservicios-usuarios ./microservicios-usuarios
+docker build  --no-cache -t  hellcaller/practica:microservicios-gateway ./microservicios-gateway
+docker build  --no-cache -t  hellcaller/practica:frontend ./frontEnd
